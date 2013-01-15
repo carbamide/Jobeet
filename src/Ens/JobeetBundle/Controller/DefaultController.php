674d04aeb8e5caadf68e3a9dc3d2e5a0e5beff31
Josh Barrow <josh@jukaela.com>
2013-01-15 16:45:49 -0600

    Add authentication to admin section

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -3,11 +3,31 @@
 namespace Ens\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {        
         return $this->render('EnsJobeetBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SECURITYCONTEXT::AUTHENTICATION_ERROR);
+        }
+        else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        
+        return $this->render('EnsJobeetBundle:Default:login.html.twig', array(
+        'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+        'error' => $error,
+        ));
     }
 }
